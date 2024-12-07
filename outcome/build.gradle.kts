758ffa5ad3c47df9bf102cbef00e543dc3334e70
Jack Norris <hamuelxd@gmail.com>
2024-12-07 23:24:58 +0800

    Removed Android config

--- outcome/build.gradle.kts
+++ outcome/build.gradle.kts
@@ -1,39 +1,21 @@
 import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
-import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
-    alias(libs.plugins.androidLibrary)
     alias(libs.plugins.vanniktech.mavenPublish)
 }
 
 group = "com.thebrownfoxx"
-version = "0.0.3"
+version = "0.0.4"
 
 kotlin {
     explicitApi()
 
     jvm()
-    androidTarget {
-        publishLibraryVariants("release")
-        @OptIn(ExperimentalKotlinGradlePluginApi::class)
-        compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_1_8)
-        }
-    }
     iosX64()
     iosArm64()
     iosSimulatorArm64()
     linuxX64()
-}
-
-android {
-    namespace = "com.thebrownfoxx.outcome"
-    compileSdk = libs.versions.android.compileSdk.get().toInt()
-    defaultConfig {
-        minSdk = libs.versions.android.minSdk.get().toInt()
-    }
 }
 
 publishing {
