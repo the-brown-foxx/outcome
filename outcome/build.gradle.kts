24db7747fc83c6e38187aa8581ee32bf2924ad81
Jack Norris <hamuelxd@gmail.com>
2025-01-20 18:10:20 +0800

    Moved to Maven Central

--- outcome/build.gradle.kts
+++ outcome/build.gradle.kts
@@ -1,4 +1,7 @@
-import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
+@file:OptIn(ExperimentalWasmDsl::class)
+
+import com.vanniktech.maven.publish.SonatypeHost
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
@@ -16,18 +19,58 @@ kotlin {
     iosArm64()
     iosSimulatorArm64()
     linuxX64()
+    macosX64()
+    macosArm64()
+    mingwX64()
+    js {
+        browser()
+        nodejs()
+    }
+    wasmJs {
+        browser()
+        nodejs()
+        d8()
+    }
+    wasmWasi {
+        nodejs()
+    }
 }
 
-publishing {
-    repositories {
-        maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/the-brown-foxx/outcome")
-            credentials {
-                val properties = gradleLocalProperties(projectDir, providers)
-                username = properties.getProperty("gpr.user", System.getenv("USERNAME"))
-                password = properties.getProperty("gpr.key", System.getenv("TOKEN"))
+mavenPublishing {
+    coordinates(
+        groupId = group.toString(),
+        artifactId = "karbon",
+        version = version.toString(),
+    )
+
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+
+    signAllPublications()
+
+    pom {
+        name = "Outcome"
+        description = "An alternative to Kotlin's Result type"
+        inceptionYear = "2024"
+        url = "https://github.com/thebrownfoxx/outcome"
+
+        licenses {
+            license {
+                name = "The Apache License, Version 2.0"
+                url = "https://www.apache.org/licenses/LICENSE-2.0.txt"
+                distribution = "https://www.apache.org/licenses/LICENSE-2.0.txt"
             }
+        }
+        developers {
+            developer {
+                id = "thebrownfoxx"
+                name = "Hamuel Agulto"
+                url = "https://github.com/thebrownfoxx"
+            }
+        }
+        scm {
+            url = "https://github.com/thebrownfoxx/outcome"
+            connection = "scm:git:git://github.com/thebrownfoxx/outcome.git"
+            developerConnection = "scm:git:ssh://git@github.com/thebrownfoxx/outcome.git"
         }
     }
 }
