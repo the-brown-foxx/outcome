8b20d2d055e1079f8e843b30df6d777e23107d44
Jack Norris <hamuelxd@gmail.com>
2025-01-20 18:11:28 +0800

    Removed unused dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,4 @@
 plugins {
-    alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.kotlinMultiplatform) apply  false
     alias(libs.plugins.vanniktech.mavenPublish) apply false
 }
